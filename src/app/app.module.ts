02d938d4b62d7b07998150a4dcd15b3f76b29bf3
Mbuso Kotobe <mbuso.kotobes@gmail.com>
2022-08-31 21:56:10 +0200

    TASK: Created shift-management component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,29 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule, Routes } from '@angular/router'; 
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { ShiftsManagementComponent } from './shifts-management/shifts-management.component';
+
+const routes : Routes = [
+  { path: 'shifts-management', component: ShiftsManagementComponent },
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ShiftsManagementComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    [RouterModule.forRoot(routes)],
+    ReactiveFormsModule,
+  ],
+  exports: [
+    RouterModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/shifts-management/shifts-management.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-shifts-management',
+  templateUrl: './shifts-management.component.html',
+  styleUrls: ['./shifts-management.component.css']
+})
+export class ShiftsManagementComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
