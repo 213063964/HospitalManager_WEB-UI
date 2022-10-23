522940b612c674d019e1874fe5b50133d08cc6b6
Elvis <213063964@mycput.ac.za>
2022-10-23 13:31:33 +0200

    Front end done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,41 +1,36 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
-
 import {LoginComponent} from "./login/login.component";
 import {AdminListViewComponent} from "./admin-list-view/admin-list-view.component";
 import {AdminListViewRolesComponent} from "./admin-list-view-roles/admin-list-view-roles.component";
-import { ShiftsManagementComponent } from "./shifts-management/shifts-management.component";
+
 import {PatientUserInterfaceComponent} from "./patient-user-interface/patient-user-interface.component";
 import {AddressUserInterfaceComponent} from "./address-user-interface/address-user-interface.component";
+
 import {WardListViewComponent} from "./ward-list-view/ward-list-view.component";
-import {AppointmentComponent} from "./appointment-user-interface/appointment.component";
+
 
 const routes: Routes = [
   {path:'', redirectTo: 'login', pathMatch: 'full'},
   {path: 'login', component: LoginComponent},
   {path: 'admin-list-employees', component: AdminListViewComponent},
   {path: 'admin-list-roles', component: AdminListViewRolesComponent},
+
   {path: 'patient-user-interface', component: PatientUserInterfaceComponent},
-  {path: 'address-user-interface', component: AddressUserInterfaceComponent},
-  {path: 'user-list-wards', component: WardListViewComponent},
-  {path: 'shifts-management', component: ShiftsManagementComponent},
-  {path: 'appointment-user-interface', component: AppointmentComponent}
+  {path: 'address-user-interface', component: AddressUserInterfaceComponent}
+
+  {path: 'user-list-wards', component: WardListViewComponent}
+
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-
 export class AppRoutingModule { }
-export const routingComponents = [
-  LoginComponent,
-  AdminListViewComponent,
-  AdminListViewRolesComponent,
-  ShiftsManagementComponent,
-  PatientUserInterfaceComponent,
-  AddressUserInterfaceComponent,
-  WardListViewComponent,
-  AppointmentComponent
-];
+
+export const routingComponents = [LoginComponent, AdminListViewComponent,
+  AdminListViewRolesComponent,PatientUserInterfaceComponent, AddressUserInterfaceComponent]
+
+export const routingComponents = [LoginComponent, AdminListViewComponent, AdminListViewRolesComponent, WardListViewComponent]
 
---
+++ src/app/services/medication.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {Medication} from "../models/medication.model";
+import {environment} from "../../environments/environment";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MedicationService {
+
+  private apiServerUrl = environment.apiBaseUrl;
+
+  constructor(private http: HttpClient) { }
+
+  public getMedications(): Observable<Medication[]> {
+    return this.http.get<Medication[]>(`${this.apiServerUrl}medication/find-all`)
+  }
+
+  public addMedication(medication: Medication): Observable<Medication> {
+    return this.http.post<Medication>(`${this.apiServerUrl}medication/save`, medication)
+  }
+
+  public readMedication(medicationId: string): Observable<Medication> {
+    return this.http.get<Medication>(`${this.apiServerUrl}medication/read/${medicationId}`)
+  }
+
+  public deleteMedication(medicationId: string): Observable<void> {
+    return this.http.delete<void>(`${this.apiServerUrl}medication/delete/${medicationId}`)
+  }
+}
